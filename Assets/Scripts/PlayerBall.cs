6b83700e02a760071e8535bf163c7ff68b0cf147
FreedLOW <0lsidl0@gmail.com>
2021-10-13 01:35:28 +0300

    hot fix №2

--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -6,7 +6,10 @@ public class PlayerBall : MonoBehaviour
 {
     [SerializeField] private float forceSpeed = 10f;
     [SerializeField] GameObject stick;  //его отодвигать
+    [SerializeField] Transform ghostBall;
+    private Transform targetBall;
 
+    private const float Speed = 10f;
     private Rigidbody ballBody;
     private LineRenderer lineRenderer;
 
@@ -17,35 +20,106 @@ public class PlayerBall : MonoBehaviour
     private void Start()
     {
         ballBody = GetComponent<Rigidbody>();
-        //lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = GetComponent<LineRenderer>();
 
-        Invoke("AddForceToBall", 2f);
+        //Invoke("AddForceToBall", 2f);
     }
 
     private void FixedUpdate()
     {
-
+        DrawLine();
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Debug.DrawLine(transform.position, ball.position);
-        Gizmos.DrawSphere(ball.position, 1f);
-    }
-
-    private void AddForceToBall()
+    public void AddForceToBall(Vector3 forceDirection, float force)
     {
         //рассчитывать направление и его примень к скорости:
-        direction = -Vector3.forward;
+        //direction = -Vector3.forward;
+        direction = forceDirection;
 
         //ballBody.AddForce(-Vector3.forward * forceSpeed);
         //ballBody.velocity = direction * forceSpeed;
+        ballBody.velocity = direction * (Speed * force);
     }
 
     private void DrawLine()
     {
+        Ray playerBallRay = new Ray(transform.position, -transform.forward);
+        Ray ballRay;
+        RaycastHit hit;
+        var hitBall = Hit(playerBallRay, out ballRay, out hit);
 
+        if (hitBall)
+        {
+            Debug.DrawLine(playerBallRay.origin, hit.point);
+            Debug.DrawLine(ballRay.origin, ballRay.origin + 3 * ballRay.direction);
+
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, playerBallRay.origin);
+            lineRenderer.SetPosition(2, hit.point);
+            lineRenderer.SetPosition(3, ballRay.origin);
+            lineRenderer.SetPosition(4, ballRay.origin + 3 * ballRay.direction);
+        }
+
+        //Ray origin = new Ray(transform.position, -transform.forward);
+        //Ray left = new Ray(transform.position + (-transform.right * 0.5f), -transform.forward);
+        //Ray right = new Ray(transform.position + (transform.right * 0.5f), -transform.forward);
+
+        //bool hiting = AimToBall(origin);
+        //if (!hiting)
+        //{
+        //    hiting = AimToBall(left);
+
+        //    if (!hiting)
+        //        hiting = AimToBall(right);
+        //}
+    }
+
+    bool Hit(Ray ray, out Ray deflected, out RaycastHit hit)
+    {
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.GetComponent<Ball>())
+            {
+                lineRenderer.enabled = true;
+                Vector3 normal = hit.normal;
+                Vector3 deflect = Vector3.Reflect(ray.direction, normal);
+                deflected = new Ray(hit.point, deflect);
+                ghostBall.position = hit.point;
+                return true;
+            }
+            else
+            {
+                lineRenderer.enabled = false;
+                ghostBall.position = Vector3.zero;
+            }
+        }
+
+        deflected = new Ray(Vector3.zero, Vector3.zero);
+        return false;
+    }
+
+    bool AimToBall(Ray ray)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit))
+        {
+            targetBall = hit.transform;
+
+            Debug.DrawLine(ray.origin, hit.point);
+
+            Vector3 pointer = transform.position - hit.transform.position;
+
+            float b = Vector3.Dot(-transform.right, pointer);
+            float a = Mathf.Sqrt(Mathf.Pow(2 * 0.5f, 2) - b * b);
+
+            ghostBall.position = hit.transform.position;
+            Vector3 pos = transform.TransformPoint(new Vector3(-b, transform.position.y, -a));
+            ghostBall.Translate(pos);
+
+            return true;
+        }
+
+        return false;
     }
 
     public void ResetValues()
---
+++ Assets/Scripts/StickController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickController : MonoBehaviour
+{
+    [SerializeField] private PlayerBall playerBall;
+    [SerializeField] float followSpeed = 2f;
+
+    private Vector3 direction;
+
+    private float distanceForce;
+    private float distance;
+
+    private void Start()
+    {
+        distance = Mathf.Clamp(-3 / 2, -3, 0);
+    }
+
+    private void Update()
+    {
+        GetDirection();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerBall>())
+        {
+            playerBall.AddForceToBall(-Vector3.forward, distanceForce);
+        }
+    }
+
+    private void GetDirection()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            //if (touch.phase == TouchPhase.Moved)
+            //{
+            //    transform.position = new Vector3(transform.position.x + touch.deltaPosition.x * followSpeed,
+            //                                    transform.position.y,
+            //                                    transform.position.z + touch.deltaPosition.y * followSpeed);
+            //    transform.LookAt(playerBall.transform);
+            //}
+        }
+    }
+}
