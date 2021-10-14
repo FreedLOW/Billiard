94ce694965cf7ba650a404c569f074acaeec6014
FreedLOW <0lsidl0@gmail.com>
2021-10-14 13:37:25 +0300

    hot fix №4

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -11,9 +11,20 @@ public class Ball : MonoBehaviour
         thisBallBody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        ClampPosition();
+    }
+
     void StopVelocity()
     {
         thisBallBody.velocity = Vector3.zero;
         thisBallBody.angularVelocity = Vector3.zero;
+    }
+
+    private void ClampPosition()
+    {
+        var yPos = Mathf.Clamp(transform.position.y, 31, 34);
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
     }
 }
--- Assets/Scripts/PlayerBall.cs
+++ Assets/Scripts/PlayerBall.cs
@@ -9,7 +9,7 @@ public class PlayerBall : MonoBehaviour
     [SerializeField] Transform ghostBall;
     private Transform targetBall;
 
-    private const float Speed = 10f;
+    private const float Speed = 5f;
     private Rigidbody ballBody;
     private LineRenderer lineRenderer;
 
@@ -19,6 +19,7 @@ public class PlayerBall : MonoBehaviour
     [SerializeField] float rotationSpeed = 0.5f;
 
     [SerializeField] private bool hitBall;
+    [SerializeField] private bool canHit;
 
     [SerializeField] Transform ball;
 
@@ -28,11 +29,9 @@ public class PlayerBall : MonoBehaviour
         lineRenderer = GetComponent<LineRenderer>();
 
         originRotation = transform.rotation;
-
-        //Invoke("AddForceToBall", 2f);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         //DrawLine();
         //ClampPosition();
@@ -45,15 +44,12 @@ public class PlayerBall : MonoBehaviour
         transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
     }
 
-    public void AddForceToBall(float force)
+    private void AddForceToBall(float force)
     {
         //рассчитывать направление и его примень к скорости:
-        //direction = -Vector3.forward;
-        //direction = forceDirection;
 
         //ballBody.AddForce(direction * (Speed * force));
-        //ballBody.velocity = direction * forceSpeed;
-        ballBody.velocity = direction * (Speed * force);
+        ballBody.velocity = direction * (Speed * force);// * Time.deltaTime);
     }
 
     private void KickBall()
@@ -63,6 +59,8 @@ public class PlayerBall : MonoBehaviour
             DrawLine();
 
             var touch = Input.GetTouch(0);
+            var touchposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var worldTouchPosition = new Vector3(touchposition.x, transform.position.y, touchposition.z);
             if (touch.phase == TouchPhase.Began)
             {
                 ResetValues();
@@ -72,20 +70,26 @@ public class PlayerBall : MonoBehaviour
             if (touch.phase == TouchPhase.Moved)
             {
                 //тут ещё настроить вращение по У, может блочит вращени по x, z:
-                horizontalRotation += touch.deltaPosition.y * rotationSpeed * Time.deltaTime;
-                Quaternion rotationY = Quaternion.AngleAxis(horizontalRotation, Vector3.up);
-                transform.rotation = originRotation * rotationY;
+
+                //horizontalRotation += touch.deltaPosition.y * rotationSpeed * Time.deltaTime;
+                //Quaternion rotationY = Quaternion.AngleAxis(horizontalRotation, Vector3.up);
+                //transform.rotation = originRotation * rotationY;
+
+                //var rotY = Quaternion.Euler(0f, touch.deltaPosition.x * rotationSpeed * Time.deltaTime, 0f);
+                //transform.rotation = rotY * transform.rotation;
+
+                transform.LookAt(worldTouchPosition);
             }
 
-            if (touch.phase == TouchPhase.Ended && hitBall)
+            if (touch.phase == TouchPhase.Ended && canHit)
             {
                 //тут вычитывать силу от шара до точки натяга и прикладывать силу в соответствующем направлении:
 
                 //Vector3 forceDirection = (transform.position + (Vector3)touch.deltaPosition).normalized;
 
                 //правильно рассчитать силу:
-                //var force = transform.position.z + touch.deltaPosition.y;
-                AddForceToBall(4f);
+                forceSpeed = Vector3.Distance(transform.position, worldTouchPosition);
+                AddForceToBall(forceSpeed);
             }
         }
         else
@@ -98,12 +102,15 @@ public class PlayerBall : MonoBehaviour
     private void DrawLine()
     {
         //выпускать луч в противоположную сторону тача:
-        //if (Input.touchCount > 0)
-        //{
-        //    Touch touch = Input.GetTouch(0);
-        //    Ray playerRay = new Ray(transform.position, -touch.position);
-        //    Debug.DrawRay(transform.position, -touch.position, Color.black);
-        //}
+        if (Input.touchCount > 0)
+        {
+            //Touch touch = Input.GetTouch(0);
+            //var touch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //var mewTouch = new Vector3(touch.x, transform.position.y, touch.z);
+            //Debug.Log("ge = " + mewTouch);
+            //Ray playerRay = new Ray(transform.position, mewTouch);
+            //Debug.DrawLine(transform.position, mewTouch, Color.black);
+        }
 
         Ray playerBallRay = new Ray(transform.position, -transform.forward);
         Ray ballRay;
@@ -111,10 +118,12 @@ public class PlayerBall : MonoBehaviour
         hitBall = Hit(playerBallRay, out ballRay, out hit);
         direction = playerBallRay.direction;
 
-        if (hitBall)
+        if (hitBall && hit.collider.GetComponent<Ball>())
         {
+            canHit = true;
+
             //убрать это потом:
-            Debug.DrawLine(playerBallRay.origin, hit.point);
+            Debug.DrawLine(playerBallRay.origin, hit.point, Color.red);
             Debug.DrawLine(ballRay.origin, ballRay.origin + 3 * ballRay.direction);
 
             lineRenderer.SetPosition(0, transform.position);
@@ -123,61 +132,26 @@ public class PlayerBall : MonoBehaviour
             lineRenderer.SetPosition(3, ballRay.origin);
             lineRenderer.SetPosition(4, ballRay.origin + 3 * ballRay.direction);
         }
-
-        //Ray origin = new Ray(transform.position, -transform.forward);
-        //Ray left = new Ray(transform.position + (-transform.right * 0.5f), -transform.forward);
-        //Ray right = new Ray(transform.position + (transform.right * 0.5f), -transform.forward);
-
-        //bool hiting = AimToBall(origin);
-        //if (!hiting)
-        //{
-        //    hiting = AimToBall(left);
-
-        //    if (!hiting)
-        //        hiting = AimToBall(right);
-        //}
+        else
+        {
+            lineRenderer.enabled = false;
+            canHit = false;
+        }
     }
 
     bool Hit(Ray ray, out Ray deflected, out RaycastHit hit)
     {
         if(Physics.Raycast(ray, out hit))
         {
-            if (hit.collider.GetComponent<Ball>())
-            {
-                lineRenderer.enabled = true;
-                Vector3 normal = hit.normal;
-                Vector3 deflect = Vector3.Reflect(ray.direction, normal);
-                deflected = new Ray(hit.point, deflect);
-                ghostBall.position = hit.point;
-                return true;
-            }
-        }
-
-        deflected = new Ray(Vector3.zero, Vector3.zero);
-        return false;
-    }
-
-    bool AimToBall(Ray ray)
-    {
-        RaycastHit hit;
-        if(Physics.Raycast(ray,out hit))
-        {
-            targetBall = hit.transform;
-
-            Debug.DrawLine(ray.origin, hit.point);
-
-            Vector3 pointer = transform.position - hit.transform.position;
-
-            float b = Vector3.Dot(-transform.right, pointer);
-            float a = Mathf.Sqrt(Mathf.Pow(2 * 0.5f, 2) - b * b);
-
-            ghostBall.position = hit.transform.position;
-            Vector3 pos = transform.TransformPoint(new Vector3(-b, transform.position.y, -a));
-            ghostBall.Translate(pos);
-
+            lineRenderer.enabled = true;
+            Vector3 normal = hit.normal;
+            Vector3 deflect = Vector3.Reflect(ray.direction, normal);
+            deflected = new Ray(hit.point, deflect);
+            ghostBall.position = hit.point;
             return true;
         }
 
+        deflected = new Ray(Vector3.zero, Vector3.zero);
         return false;
     }
 
